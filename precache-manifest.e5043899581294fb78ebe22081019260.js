f2230615261c0960314bfdc372a9d054be0dfed5
42b883 <42b883@gmail.com>
2019-03-20 17:37:32 +0600

    Updates

---
+++ precache-manifest.e5043899581294fb78ebe22081019260.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0c27c5a324e5e45e9fe",
+    "url": "/42b883.github.io/casablanca/static/css/main.1a77a8e2.chunk.css"
+  },
+  {
+    "revision": "e0c27c5a324e5e45e9fe",
+    "url": "/42b883.github.io/casablanca/static/js/main.e0c27c5a.chunk.js"
+  },
+  {
+    "revision": "158e85eb7437b7566443",
+    "url": "/42b883.github.io/casablanca/static/js/runtime~main.158e85eb.js"
+  },
+  {
+    "revision": "464f2903519fb3068afc",
+    "url": "/42b883.github.io/casablanca/static/js/2.464f2903.chunk.js"
+  },
+  {
+    "revision": "49d29e31f953ccd0b36417c8a0f04cdf",
+    "url": "/42b883.github.io/casablanca/static/media/person1.49d29e31.png"
+  },
+  {
+    "revision": "8ef7fff061662d4148c856ed5cff1e81",
+    "url": "/42b883.github.io/casablanca/static/media/person2.8ef7fff0.png"
+  },
+  {
+    "revision": "1555f0f2d48901f114f6e4c93da64dfc",
+    "url": "/42b883.github.io/casablanca/static/media/person3.1555f0f2.png"
+  },
+  {
+    "revision": "18b7a2b3c998524818dcd594b7c9ba67",
+    "url": "/42b883.github.io/casablanca/static/media/header_bg.18b7a2b3.jpg"
+  },
+  {
+    "revision": "a2e89d2161d38403d7c6dd25f5792dce",
+    "url": "/42b883.github.io/casablanca/static/media/imac.a2e89d21.jpg"
+  },
+  {
+    "revision": "243a6a763d877bf84e809c8f1c886962",
+    "url": "/42b883.github.io/casablanca/static/media/projects_branding1.243a6a76.jpg"
+  },
+  {
+    "revision": "7da8c46e7e74accbaff033d01116155c",
+    "url": "/42b883.github.io/casablanca/static/media/projects_branding2.7da8c46e.jpg"
+  },
+  {
+    "revision": "2b1a67f8373a39dafc35c89aa326dba6",
+    "url": "/42b883.github.io/casablanca/static/media/projects_branding3.2b1a67f8.jpg"
+  },
+  {
+    "revision": "baac1e7e4f8769a4aca1d8f643676319",
+    "url": "/42b883.github.io/casablanca/static/media/projects_illustrations1.baac1e7e.jpg"
+  },
+  {
+    "revision": "4c22dc59cb66006ca8f25adcf8b36124",
+    "url": "/42b883.github.io/casablanca/static/media/projects_webDesign1.4c22dc59.jpg"
+  },
+  {
+    "revision": "33d168702df2f5da9c62084bd68e6102",
+    "url": "/42b883.github.io/casablanca/static/media/projects_mobileUI1.33d16870.jpg"
+  },
+  {
+    "revision": "a9250489bcc7902e5fdf0bd10a87ab46",
+    "url": "/42b883.github.io/casablanca/static/media/ipad.a9250489.jpg"
+  },
+  {
+    "revision": "2ba06c68fdc9ee9c4b116375ee269d0d",
+    "url": "/42b883.github.io/casablanca/static/media/Introduction_bg.2ba06c68.jpg"
+  },
+  {
+    "revision": "fb8b5418cd9ab2202086f4a9aa801b45",
+    "url": "/42b883.github.io/casablanca/static/media/clients_bg.fb8b5418.jpg"
+  },
+  {
+    "revision": "01d0c6466f75e032c07900a78b07a772",
+    "url": "/42b883.github.io/casablanca/static/media/getintouch_bg.01d0c646.jpg"
+  },
+  {
+    "revision": "1e2939c21ba4c51f6bcd99cf04255823",
+    "url": "/42b883.github.io/casablanca/static/media/iphone.1e2939c2.jpg"
+  },
+  {
+    "revision": "42619b8181810cae8e174e209dee9b12",
+    "url": "/42b883.github.io/casablanca/static/media/footer__icon.42619b81.jpg"
+  },
+  {
+    "revision": "2cf628283fb0b4be2d92de8e7f519dfd",
+    "url": "/42b883.github.io/casablanca/index.html"
+  }
+];
